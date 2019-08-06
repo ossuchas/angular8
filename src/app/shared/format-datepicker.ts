1426fcde9730f3da9e8b1a829b31ec08f8c69190
ossuchas <suchat.s14102526@gmail.com>
2019-08-07 05:53:10 +0700

    modify

--- src/app/shared/format-datepicker.ts
+++ src/app/shared/format-datepicker.ts
@@ -2,6 +2,18 @@ import { NativeDateAdapter } from '@angular/material';
 import { MatDateFormats } from '@angular/material/core';
 
 export class AppDateAdapter extends NativeDateAdapter {
+  parse(value: any): Date | null {
+    if ((typeof value === 'string') && (value.indexOf('/') > -1)) {
+      const str = value.split('/');
+      const year = Number(str[0]);
+      const month = Number(str[1]) - 1;
+      const date = Number(str[2]);
+      return new Date(year, month, date);
+    }
+    const timestamp = typeof value === 'number' ? value : Date.parse(value);
+    return isNaN(timestamp) ? null : new Date(timestamp);
+  }
+
   format(date: Date, displayFormat: Object): string {
     if (displayFormat === 'input') {
       let day: string = date.getDate().toString();
